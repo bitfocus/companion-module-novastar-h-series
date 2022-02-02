b70050b65be865680f11afa01f5849da84b6cb51
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:47 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ instance.prototype.init_presets = function() {
 instance.prototype.actions = function(system) {
     var self = this;
 
-    self.system.emit('instance_actions', self.id, {
+    self.setActions({
 
 //----------------------------RECALL PRESETS------------------------------------
 
